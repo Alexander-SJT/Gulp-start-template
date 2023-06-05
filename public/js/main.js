7783862154210d2619a6d1892c1fca610f4d7f98
Alexander-SJT <alexander3post@gmail.com>
2023-06-05 11:01:17 +0300

    3-4day

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,1 @@
-import burgeMenu from"./burger.js";burgeMenu();
+import burgeMenu from"./burger.js";import Swiper from"./swiper-bundle.esm.browser.min.js";const swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,slidesPerView:5,spaceBetween:16,breakpoints:{320:{slidesPerView:2,spaceBetween:20},500:{slidesPerView:3},1230:{slidesPerView:4,spaceBetween:30},1500:{slidesPerView:5,spaceBetween:40}},pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});burgeMenu();
